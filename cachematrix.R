ea556bf62b21b79f5b464b60d43476dbcbb8f6a0
Mark Promislow <MarkPromislow@gmail.com>
2014-07-16 08:14:17 -0400

    filled out functions

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,44 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m_matrix = matrix()) {
+    m_inverse <- NULL
+    set <- function(m) {
+        m_matrix <- m
+        m_inverse <- NULL;
+    }
+    get <- function() { m_matrix }
+    setinverse <- function(inverse) { m_inverse <<-inverse }
+    getinverse <- function() { m_inverse }
+    
+    list(set = set, get = get, setInverse = setinverse, getInverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if(is.null(inverse)) {
+        message("cacheSolve: solve for matrix inverse")
+        inverse <- solve(x$get())
+        x$setInverse(inverse)
+    }
+    inverse
 }
+
+## create a test matrix with random numbers
+n <- 8
+testMatrix <- matrix(data = runif(n * n, 0, 10), nrow = n, ncol = n, byrow = TRUE)
+
+cacheMatrix <- makeCacheMatrix(testMatrix)
+inverseMatrix <- cacheSolve(cacheMatrix)
+identical(inverseMatrix, cacheSolve(cacheMatrix))
+identical(cacheMatrix$getInverse(), inverseMatrix)
+
+## verify results
+testMatrix
+cacheMatrix$getInverse()
+testMatrix %*% inverseMatrix
+
